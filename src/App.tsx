edc40ad2ca9d5f9cb5e1848409da88dd35a5deea
U0L9511 <estclio09@gmail.com>
2022-07-11 17:15:06 +0300

    Add global context to avoid props drilling

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
-import { useState } from "react";
+import React from "react";
 
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
+import TodoContextProvider from "./store/todo-context";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (id: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== id);
-    });
-  };
-
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+    <TodoContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodoContextProvider>
   );
 }
 
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,27 +1,25 @@
-import { useRef } from 'react';
+import { useRef, useContext } from "react";
+import { TodoContext } from "../store/todo-context";
+import classes from "./NewTodo.module.css";
 
-import classes from './NewTodo.module.css';
-
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
+  const todoCtx = useContext(TodoContext);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
-
     const enteredText = todoTextInputRef.current!.value;
-
     if (enteredText.trim().length === 0) {
       // throw an error
       return;
     }
-
-    props.onAddTodo(enteredText);
+    todoCtx.addTodo(enteredText);
   };
 
   return (
     <form onSubmit={submitHandler} className={classes.form}>
-      <label htmlFor='text'>Todo text</label>
-      <input type='text' id='text' ref={todoTextInputRef} />
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
